83340df9cd35131c225649274164f8777ca045ee
James Newton-King <james@newtonking.com>
2021-01-28 12:43:09 +1300

    Add connection logging for multiplexed connections

--- src/Servers/Kestrel/Core/src/Middleware/LoggingConnectionMiddleware.cs
+++ src/Servers/Kestrel/Core/src/Middleware/LoggingConnectionMiddleware.cs
@@ -2,26 +2,35 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Pipelines;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Connections.Experimental;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
 {
     internal class LoggingConnectionMiddleware
     {
-        private readonly ConnectionDelegate _next;
+        private readonly ConnectionDelegate? _next;
+        private readonly MultiplexedConnectionDelegate? _multiplexedNext;
         private readonly ILogger _logger;
 
-        public LoggingConnectionMiddleware(ConnectionDelegate next, ILogger logger)
+        public LoggingConnectionMiddleware(ConnectionDelegate? next, MultiplexedConnectionDelegate? multiplexedNext, ILogger logger)
         {
-            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _next = next;
+            _multiplexedNext = multiplexedNext;
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task OnConnectionAsync(ConnectionContext context)
         {
+            Debug.Assert(_next != null);
+
             var oldTransport = context.Transport;
 
             try
@@ -36,6 +45,58 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal
             finally
             {
                 context.Transport = oldTransport;
+            }
+        }
+
+        public async Task OnConnectionAsync(MultiplexedConnectionContext context)
+        {
+            Debug.Assert(_multiplexedNext != null);
+
+            await _multiplexedNext(new LoggingMultiplexedConnectionContext(context, _logger));
+        }
+
+        private class LoggingMultiplexedConnectionContext : MultiplexedConnectionContext
+        {
+            private readonly MultiplexedConnectionContext _inner;
+            private readonly ILogger _logger;
+
+            public LoggingMultiplexedConnectionContext(MultiplexedConnectionContext inner, ILogger logger)
+            {
+                _inner = inner;
+                _logger = logger;
+            }
+
+            public override string ConnectionId { get => _inner.ConnectionId; set => _inner.ConnectionId = value; }
+            public override IFeatureCollection Features => _inner.Features;
+            public override IDictionary<object, object?> Items { get => _inner.Items; set => _inner.Items = value; }
+
+            public override void Abort()
+            {
+                _inner.Abort();
+            }
+
+            public override void Abort(ConnectionAbortedException abortReason)
+            {
+                _inner.Abort(abortReason);
+            }
+
+            public override async ValueTask<ConnectionContext?> AcceptAsync(CancellationToken cancellationToken = default)
+            {
+                var context = await _inner.AcceptAsync(cancellationToken);
+                if (context != null)
+                {
+                    context.Transport = new LoggingDuplexPipe(context.Transport, _logger);
+                }
+                return context;
+            }
+
+            public override async ValueTask<ConnectionContext> ConnectAsync(IFeatureCollection? features = null, CancellationToken cancellationToken = default)
+            {
+                var context = await _inner.ConnectAsync(features, cancellationToken);
+
+                context.Transport = new LoggingDuplexPipe(context.Transport, _logger);
+
+                return context;
             }
         }
 
